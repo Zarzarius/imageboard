5d745c2755e7531bc91bd6088fa69c142420ce6b
Azael Alonso Campana <zarzarius@gmail.com>
2021-03-28 21:48:29 +0200

    .env

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -13,8 +15,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://zarzarius:zarzarius@cluster0.vq4u4.mongodb.net/mentoring_db?retryWrites=true&w=majority";
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vq4u4.mongodb.net/mentoring_db?retryWrites=true&w=majority`;
 
 const PORT = process.env.PORT || 5000;
 
